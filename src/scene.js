04c98f00792e9abbd9b0184ffe15d48c6cd86389
josai <josiah_swaim@hotmail.com>
2021-06-24 15:42:21 -0600

    created basic stub structure for the primary scene

--- src/scene.js
+++ src/scene.js
@@ -1,6 +1,27 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from './node_modules/three/build/three.module.js'
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js'
 
-function init() {
+function init () {
+  // Scene setup
+  const scene = new THREE.Scene()
+  return scene
 };
-
-init();
+function buildCamera () {
+  const mm = 75 // lens
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const camera = new THREE.PerspectiveCamera(mm, width / height, 0.1, 1000)
+  const controls = new OrbitControls(camera, renderer.domElement)
+  return camera
+};
+function buildRenderer () {
+  const renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: true })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.shadowMap.enabled = true
+  document.body.appendChild(renderer.domElement)
+  return renderer
+};
+function main () {
+  const scene = init()
+  const renderer = buildRenderer()
+};
