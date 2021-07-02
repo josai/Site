31aaf9d4baae132a75bbfe1191e5fc641914c9d6
Jo <josiah_swaim@hotmail.com>
2021-07-02 13:29:45 -0600

    added text dialog box and splash screen

--- js/scene.js
+++ js/scene.js
@@ -1,5 +1,16 @@
-// orbit controls for testing/viewing
 
+function dialog(txt) {
+  var i = 0;
+  var speed = 50; /* The speed/duration of the effect in milliseconds */
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("dialogText").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+  typeWriter()
+}
 
 function loadMeshes () {
   const meshes = [testCube()];
@@ -53,9 +64,17 @@ function loadLights () {
   lights.push(new THREE.AmbientLight(0xFFFFFF, intensity / 4));
   return lights
 };
-function init (objects) {
+function init (objects, renderer) {
   // Scene setup
   const scene = new THREE.Scene()
+  //Load background texture
+  const texloader = new THREE.TextureLoader();
+  texloader.load('models/background.jpg' , function(texture)
+              {
+                const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+                rt.fromEquirectangularTexture(renderer, texture);
+                scene.background = rt.texture;
+              });
   for (let i = 0; i < objects.length; i++) {
     console.log(objects[i])
     scene.add(objects[i])
@@ -70,6 +89,7 @@ function testCube () {
   const cube = new THREE.Mesh(geometry, material)
   cube.scale.set(2, 2, 2)
   cube.name = 'testcube'
+
   return cube
 };
 function buildCamera () {
@@ -77,7 +97,7 @@ function buildCamera () {
   const width = window.innerWidth
   const height = window.innerHeight
   const camera = new THREE.PerspectiveCamera(mm, width / height, 0.1, 1000)
-  camera.position.set(0, 0, 6)
+  camera.position.set(0.7266766457148011, 2.234444172194488, 24.567981456895367)
   return camera
 };
 function buildRenderer () {
@@ -95,7 +115,6 @@ function buildComposer(renderer, scene, cam) {
   const renderPass = new POSTPROCESSING.RenderPass( scene, cam);
   let vignette = new POSTPROCESSING.VignetteEffect();
   let bloom = new POSTPROCESSING.BloomEffect({"intensity":2});
-
   let dof = new POSTPROCESSING.DepthOfFieldEffect(cam, {focusDistance: 0.02, focalLength: 0.058, bokehScale: 3.0});
 
   const effectPass = new POSTPROCESSING.EffectPass(cam, dof, bloom, vignette,);
@@ -108,27 +127,19 @@ function buildComposer(renderer, scene, cam) {
 function main () {
 
   const sceneObjects = [].concat(loadMeshes(), loadLights())
-  const scene = init(sceneObjects)
-
-  //Load background texture
-  const texloader = new THREE.TextureLoader();
-  texloader.load('models/background.jpg' , function(texture)
-              {
-                const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
-                rt.fromEquirectangularTexture(renderer, texture);
-                scene.background = rt.texture;
-              });
 
   const cam = buildCamera()
   const renderer = buildRenderer()
   renderer.shadowMap.enabled = true;
+  const scene = init(sceneObjects, renderer);
 
   const composer = buildComposer(renderer, scene, cam);
   const controls = new THREE.OrbitControls( cam, renderer.domElement );
 
-
+  dialog('Hello and welcome to my CV!')
   const animate = function () {
       requestAnimationFrame(animate)
+      //console.log(cam.rotation);
       composer.render(scene, cam)
   };
   animate()
